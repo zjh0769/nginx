dff98600766af580c0aacc3144055eb9d1eac38a
zjh0769 <35723714@qq.com>
2019-01-26 09:54:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum  -y install gcc gcc-c++ automake pcre pcre-devel zlip zlib-devel openssl openssl-devel 
+apt-get update
+apt-get -y --force-yes install rpm
 cd /usr/test
 rpm -ivh nginx-1.14.0-1.el7_4.ngx.x86_64.rpm
 
